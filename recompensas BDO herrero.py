081c1cae07c3ad9ec5ad9224504507029e0efa45
jymnils2 <86804327+jymnils2@users.noreply.github.com>
2024-04-11 13:02:41 -0400

    Add files via upload

---
+++ recompensas BDO herrero.py
@@ -0,0 +1,42 @@
+bulk order rewards
+item                        puntos
+martillo con 250 usos       10
+Pala con 150 a 200 usos     10
+picota con 150 a 200 usos   25
+scroll deed The Weaponsmith 25
+scroll deed The Armorer     100
+mining gloves 101           100
+mining gloves 103           200
+prospector tool 50 usos     200
+gargole picota 100 a 125    200
+smelter talisman Golden     350
+master talisman 10 cargas   400
+mining gloves 105           450
+polvo de fortificacion 10   450
+smelter talisman Agapite    475
+rock hammer granite         500
+dull copper runic 50 cargas 500
+smelter talisman Verite     525
+shadow iron runic 45 cargas 500
+master talisman 25 cargas   550
+smelter talisman Valorite   575
+mapa de mina con oro        600
+blacksmith scroll 105       625
+copper runic 40 cargas      650
+blacksmith scroll 110       675
+bronze runic 35 cargas      700
+ancient hammer +10 600 usos 750
+blacksmith scroll 115       800
+ancient hammer +15 600 usos 850
+mapa de mina con agapita    850
+blacksmith scroll 120       900
+master talisman 50 cargas   900
+gold runic 30 cargas        950
+mapa de mina con verita     950
+ancient hammer +30 600 usos 1000
+agapite runic 25 cargas     1050
+mapa de mina con valorita   1050
+ancient hammer +60 600 usos 1100
+verite runic 20 cargas      1150
+valorite runic 15 cargas    1150
+
