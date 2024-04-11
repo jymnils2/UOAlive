5448b1c4162b665f8295b50e70ad666d1b9c78ab
jymnils2 <86804327+jymnils2@users.noreply.github.com>
2024-04-11 18:25:04 -0400

    Update detecthiden.py

--- detecthiden.py
+++ detecthiden.py
@@ -10,6 +10,10 @@
 #                tu casa o el banco son excelentes opciones
 #                pero no elijas un lugar peligroso con npc agresivos
 #                la libreria del UOAlive es ideal para entrenar esta habilidad y ganar Britcoins
+#                este script puede ser usado como molde para entrenar muchas otras actividades como por ejemplo
+#                skills gratuitas del server UOAlive arms lore, hidding(sin la linea 19 y 20), item identification taste identification
+#                otras skills animal lore, evaluate intelligence, stealing, anatomy, 
+#                solo basta con cambiar el skill de la linea 19 y el item de la linea 18 segun la skill requiera otra persona, un pet, o un item X
 ##############################################################################
 item = 0x4034EDF5           #serial de un item en el piso
 Player.UseSkill("Detect Hidden")
