14a8531dbf617d2b1d519f1a84f18e0ec00139e2
jymnils2 <86804327+jymnils2@users.noreply.github.com>
2024-04-11 18:01:09 -0400

    Add files via upload

---
+++ hidding.py
@@ -0,0 +1,13 @@
+# version 1.0
+# requisitos : haber aprendido el skill al menos a 30, pero la verdad no es necesario, solo acortaria el proceso
+# descripcion : realiza la actividad de hidding cada 10 segundos
+# configuracion este escript debe estar configurado para ejecutarse con la tecla F1
+#               este debe configurase con la opcion del razor de  Loop Mode
+#               ver "cosas que debes saber hacer"
+# consejos      cualquier lugar es bueno para practicar hidding
+                pero dicen las lenguas que hay mas ganancias en lugares donde muchos NPC te vean
+                pero no elijas un lugar peligroso con npc agresivos
+                la libreria del UOAlive es ideal para entrenar esta habilidad y ganar Britcoins
+##############################################################################
+Player.UseSkill("Hiding")
+Misc.Pause(10500)
